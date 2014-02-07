7063b645761983268ce432641817f863d8bff3a4
noahp <noah.pendleton@gmail.com>
2014-02-07 09:12:03 -0500

    Update jlink.bat

--- sw/jlink.bat
+++ sw/jlink.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\SEGGER\JLinkARM_V480e\JLinkGDBServerCL.exe" -select USB -device MKL26Z32xxx4 -if SWD -speed auto
+"C:\Program Files (x86)\SEGGER\JLinkARM_V480e\JLinkGDBServer.exe" -select USB -device MKL26Z32xxx4 -if SWD -speed auto
